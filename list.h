7b499aa8f4aab4166ed7dd4d5132dce4be203b8e
jorge rebosio <jorge.rebosio@utec.edu.pe>
2020-04-26 18:06:20 -0500

    feat(main.cpp and list.h)

--- list.h
+++ list.h
@@ -1,97 +1,257 @@
-template <typename T>
-class Node {
+#include <iostream>
+#include <ostream>
+
+
+namespace cs2100 {
+
+    template <typename Object>
+    struct Node
+    {
+        Object data;
+        Node *prev;
+        Node *next;
+
+        Node( const Object & d = Object{ }, Node * p = nullptr, Node * n = nullptr )
+                : data{ d }, prev{ p }, next{ n } { }
+
+    };
+
+
+
+
+    template <typename Object>
+    class iterator
+    {
+
+
     protected:
-        T value;
-        Node<T> * next;
+        using Node= cs2100::Node<Object>;
+
 
     public:
-        Node(void){
+        Node *current;
+
+        iterator( ) : current{ nullptr }
+        { }
+
+        Object & operator* ( )
+        { return current->data; }
+
+        iterator & operator++ ( )
+        {
+            current = current->next;
+            return *this;
         }
 
-        ~Node(void){
-        }
-};
 
-// List.h
-template <typename T>
-class List {
+        iterator & operator-- ( )
+        {
+            current = current->prev;
+            return *this;
+        }
+
+        iterator operator++ ( int )
+        {
+            iterator old = *this;
+            ++( *this );
+            return old;
+        }
+
+
+        friend iterator operator+ (const iterator &left, const int& n)
+        {
+            iterator temp=left;
+            for(int i=0;i<n;i++) temp.current=(temp.current)->next;
+            return temp;
+        }
+
+        bool operator== ( const iterator & rhs )
+        { return current == rhs.current; }
+        bool operator!= ( const iterator & rhs )
+        { return !( *this == rhs ); }\
+
+
+        iterator( Node *p ) : current{ p }
+        { }
+
+
+
+    };
+
+
+
+
+
+    template <typename Object>
+    class LinkedList
+    {
     protected:
-        Node<T> *head;
 
+        using iterator= cs2100::iterator<Object>;
+        using Node= cs2100::Node<Object>;
     public:
-        List(List){ 
-			// Constructor copia
+
+
+
+        LinkedList( )
+        { init( ); }
+
+        LinkedList( const LinkedList & rhs )
+        {
+            init( );
+            for( auto & x : rhs )
+                push_back( x );
         }
-        
-        List(T*){ 
-			//Constructor  parametro, 
-			//llena una lista a partir de un array
+        ~LinkedList( )
+        {
+            clear( );
+            delete head;
+            delete tail;
+        }
+        LinkedList & operator= ( const LinkedList & rhs )
+        {
+            LinkedList copy = rhs;
+            std::swap( *this, copy );
+            return *this;
         }
 
-        List(Node<T>*){ 
-			//Constructor por parametro, 
-			//retorna una lista con un nodo
+
+
+        iterator begin( ) const
+        { return {head->next} ; }
+
+        iterator end( ) const
+        { return { tail }; }
+
+
+        int size( ) const
+        { return theSize; }
+        bool empty( ) const
+        { return size( ) == 0; }
+
+        void clear( )
+        {
+            while( !empty( ) )
+                pop_front( );
         }
 
-        List(int){ 
-			//Constructor por parametro, 
-			//retorna un lista de randoms de tamaño n
+
+        Object & front( )
+        { return *begin( ); }
+
+        Object & back( )
+        { return *--end( ); }
+
+        void push_front( const Object & x )
+        { insert( begin( ), x ); }
+
+        void push_back( const Object & x )
+        { insert( end( ), x ); }
+
+        void pop_front( )
+        { erase( begin( ) ); }
+
+        void pop_back( )
+        { erase( --end( ) ); }
+
+        iterator insert( iterator itr, const Object & x)
+        {
+            Node *p = itr.current;
+            theSize++;
+            return { p->prev = p->prev->next = new Node{ x, p->prev, p } };
         }
 
-        List(void){ 
-			//Constructor por defecto
+        iterator erase( iterator itr )
+        {
+            Node *p = itr.current;
+            iterator retVal{ p->next };
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+            delete p;
+            theSize--;
+            return retVal;
         }
 
-        ~List(void){
+
+
+        iterator erase( iterator from, iterator to )
+        {
+            for( iterator itr = from; itr != to; )
+                itr = erase( itr );
+            return to;
         }
 
-		  // Retorna una referencia al primer elemento
-        T front(void) = 0; 
-        
-		  // Retorna una referencia al ultimo elemento
-		    T back(void) = 0; 
-        
-		  // Inserta un elemento al final
-        void push_back(const T& element) = 0; 
 
-		  // Inserta un elemento al inicio
-        void push_front(const T& element) = 0; 
+        Object& operator[] (const int& x)
+        {
+            Node* temp = head->next;
+            for(int i=0;i<x;i++)
+                temp=temp->next;
+            return temp->data;
+        };
 
-		  // Quita el ultimo elemento y retorna una referencia
-        T& pop_back(void) = 0;
 
-  		  // Quita el primer elemento y retorna una referencia
-        T& pop_front(void) = 0;  
+        LinkedList& reverse()
+        {
+            for(int i=0;i<theSize/2;i++)
+            {
+                Object temp=(*this)[i];
+                (*this)[i]=(*this)[theSize-1-i];
+                (*this)[theSize-1-i]=temp;
 
-		  // Acceso aleatorio
-        T& operator[] (const int&) = 0; 
-        
-		  // la lista esta vacia?
-        bool empty(void) = 0; 
+            }
+            return *this;
+        };
 
-		  // retorna el tamaño de la lista
-        unsigned int size(void) = 0; 
 
-		  // Elimina toda la lista
-        void clear(void) = 0; 
+        LinkedList& sort()
+        {
+            for (int i = 1; i < theSize; i++)
+            {
+                Object key = (*this)[i];
+                int j=i-1;
+                while (j >= 0 && (*this)[j] > key)
+                {
+                    (*this)[j + 1] = (*this)[j];
+                    j = j - 1;
+                }
+                (*this)[j + 1] = key;
+            }
 
-		  // Elimina un elemento en base a un puntero
-        void erase(Node<T>*) = 0; 
-        
-		  // Inserta un elemento  en base a un puntero
-        void insert(Node<T>*, const T&) = 0; 
+            return *this;
 
-		  // Elimina todos los elementos por similitud
-        void remove(const T&) = 0; 
+        };
 
-		  // ordena la lista
-        List& sort(void) = 0; 
 
-		  // invierte la lista
-        List& reverse(void) = 0; 
 
-		  // Imprime la lista con cout
-        template<typename __T>
-        inline friend std::ostream& operator<<
-			(std::ostream& , const List<__T>& ); 
+        inline friend std::ostream& operator<< (std::ostream& os, const LinkedList & l)
+        {
+            iterator temp=l.begin();
+            while(temp.current->next!=nullptr){
+                os<<*temp<<"->";
+                ++temp;
+            }
+            return os;
+        };
+
+    protected:
+        int theSize;
+        Node *head;
+        Node *tail;
+
+        void init( )
+        {
+            theSize = 0;
+            head = new Node;
+            tail = new Node;
+            head->next = tail;
+            tail->prev = head;
+        }
+
+
+
+
+    };
+
 }
+
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "list.h"
+
+using std::cout;
+using std::endl;
+
+
+
+int main(){
+
+
+    cs2100::LinkedList<int>l;
+    l.push_back(3);l.push_back(4);l.push_back(5);l.push_back(6);
+    l.push_back(7);l.push_back(8);l.push_back(9);l.push_back(10);
+    l.push_back(11);l.push_back(12);l.push_back(13);
+    cout<<"Linked List :\n\n\t\t\t\t"<<l<<"\n\n";
+
+    l.erase(l.begin()+5);
+
+    cout<<"Erase sixth element:\t\t\t"<<l<<"\n\n";
+
+
+    l.insert(l.begin()+5,69);
+
+    cout<<"Insert number 69 in sixth position: "<<l<<"\n\n\n\n\n\n";
+
+	
+}
+
